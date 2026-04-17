b01618de03a8e38eed72047a32fb696170b94090
aidazi <biosubj@gmail.com>
2026-04-17 20:00:34 +0100

    #

--- src/services/datanestApi.ts
+++ src/services/datanestApi.ts
@@ -43,7 +43,7 @@ class DatanestApiService extends ApiClient {
     if (filters.type?.length) cleanFilters.type = filters.type
     if (filters.source?.length) cleanFilters.source = filters.source
     if (Object.keys(cleanFilters).length) params.set('filter', JSON.stringify(cleanFilters))
-    const response = await this.request<any>(`/datanest/soundfragments/available-soundfragments?${params}`)
+    const response = await this.request<any>(`/soundfragments/available-soundfragments?${params}`)
     const viewData = response?.payload?.viewData ?? response?.viewData
     if (!viewData) throw new Error('Unexpected response format')
     return {
